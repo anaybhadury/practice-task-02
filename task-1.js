076e48e56c2c581c11715689285539b113417387
anaybhadury <bhadury9292@gmail.com>
2024-09-02 21:56:46 +0600

    added practice task 1

--- task-1.js
+++ task-1.js
@@ -1,3 +1,14 @@
 /***
  * ---------Practice Task-----------
+ * FREE DRINKS
+ * BURGER MORE THAN 500TK: FREE COKE
+ * ELSE COKE: 30TK
  */
+
+let burger = 500;
+if(burger >= 500){
+    console.log('free coke');
+}
+else{
+    console.log('30tk')
+}
